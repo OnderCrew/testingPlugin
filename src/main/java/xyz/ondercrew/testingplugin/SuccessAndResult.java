b8dd63e53014db0c751cfe80c1bf78acba3d1882
PMHStudio <pmhstudio.pmh@gmail.com>
2020-02-17 21:15:43 +0900

    Twitch Info Command Finished

--- src/main/java/xyz/ondercrew/testingplugin/SuccessAndResult.java
+++ src/main/java/xyz/ondercrew/testingplugin/SuccessAndResult.java
@@ -1,20 +1,21 @@
 package xyz.ondercrew.testingplugin;
 
+import org.json.JSONObject;
+
 public class SuccessAndResult {
     public boolean success;
-    public String result;
+    public JSONObject result;
 
-    public SuccessAndResult (String result) {
+    public SuccessAndResult (JSONObject result) {
         this.success = true;
         this.result = result;
     }
 
     public SuccessAndResult () {
         this.success = false;
-        this.result = "Failed";
     }
 
-    public SuccessAndResult (boolean success, String result) {
+    public SuccessAndResult (boolean success, JSONObject result) {
         this.success = success;
         this.result = result;
     }
--- src/main/java/xyz/ondercrew/testingplugin/TwitchRequest.java
+++ src/main/java/xyz/ondercrew/testingplugin/TwitchRequest.java
@@ -1,7 +1,7 @@
 package xyz.ondercrew.testingplugin;
 
+import org.bukkit.ChatColor;
 import org.json.JSONObject;
-import net.md_5.bungee.api.ChatColor;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -13,26 +13,60 @@ import org.bukkit.command.CommandSender;
 public class TwitchRequest {
     private final CloseableHttpClient httpClient = HttpClients.createDefault();
     public boolean TwitchCommand (CommandSender sender, String[] args) {
-        SuccessAndResult twitchId = username2id(args[0]);
+        SuccessAndResult twitchId = getUserInfoByUserName(args[0]);
         if (twitchId.success) {
-            sender.sendMessage(ChatColor.GREEN + "트위치 계정을 찾았습니다! : " + twitchId.result);
-        } else sender.sendMessage(ChatColor.RED + args[0] + "이라는 계정을 찾을 수 없습니다!");
+            SuccessAndResult channelInfo = getInfoById("channels", twitchId.result.getString("_id"));
+            SuccessAndResult streamInfo = getInfoById("streams", twitchId.result.getString("_id"));
+            if (channelInfo.success && streamInfo.success) {
+                boolean isStreaming = !streamInfo.result.isNull("stream");
+                if (isStreaming) streamInfo = new SuccessAndResult(streamInfo.result.getJSONObject("stream"));
+                System.out.println(twitchId.result);
+                System.out.println(channelInfo.result);
+                System.out.println(streamInfo.result);
+                String str = ChatColor.LIGHT_PURPLE + "\n" + ChatColor.BOLD + channelInfo.result.getString("display_name") + "\n"
+                            + ChatColor.RESET + ChatColor.WHITE + channelInfo.result.getString("description") + "\n====================\n"
+                            + (isStreaming ? ChatColor.GREEN + "방송중: " : ChatColor.GRAY + "방송 오프라인: ") + (channelInfo.result.isNull("status") ?
+                                "제목 미설정" : channelInfo.result.getString("status")) + "\n"
+                            + ChatColor.RESET + ChatColor.WHITE + "게임/소통: " + ChatColor.BOLD + (channelInfo.result.isNull("game") ?
+                                "게임/소통 미선택" : (isStreaming ? streamInfo.result.getString("game") : channelInfo.result.getString("game"))) + "\n"
+                            + ChatColor.RESET + "팔로워: " + ChatColor.BOLD + channelInfo.result.getNumber("followers") + "명\n"
+                            + ChatColor.RESET + "총 시청수: " + ChatColor.BOLD + channelInfo.result.getNumber("views")
+                            + (isStreaming ? ChatColor.RESET + ", 현재 시청자: " + ChatColor.BOLD + streamInfo.result.getNumber("viewers") + "명" : "") + "\n"
+                            + (isStreaming ? ChatColor.RESET + "방송 평균 FPS: " + ChatColor.BOLD + streamInfo.result.getNumber("average_fps") + ChatColor.RESET
+                            + ", 방송 딜레이: " + ChatColor.BOLD + streamInfo.result.getNumber("delay") : "");
+                sender.sendMessage(str);
+            } else {
+                sender.sendMessage(ChatColor.RED + args[0] + "(" + twitchId.result.getString("id") + ")의 정보를 구하려고 했는데, 트위치가 삐져서 안해줍니다");
+            }
+        } else sender.sendMessage(ChatColor.RED + args[0] + "이라는 계정을 찾을 수 없습니다! 손가락이 존재하는지 확인해보세요");
         return true;
     }
 
-    private SuccessAndResult username2id (String username) {
+    private SuccessAndResult getUserInfoByUserName (String username) {
         HttpGet username2idRequest = new HttpGet("https://api.twitch.tv/kraken/users?login=" + username);
         username2idRequest.addHeader("Accept", "application/vnd.twitchtv.v5+json");
-        username2idRequest.addHeader("Client-ID", System.getenv("twitchClient")); // TODO config 파일로 트위치 클라ID 받게 바꾸기
+        username2idRequest.addHeader("Client-ID", System.getenv("twitchClient")); // TODO config 파일로 트위치 클라ID 받게 바꾸기 = [1]
 
         try (CloseableHttpResponse res = httpClient.execute(username2idRequest)) {
             JSONObject resObj = new JSONObject(EntityUtils.toString(res.getEntity()));
             if (resObj.getJSONArray("users").length() > 0) {
-                String id = resObj.getJSONArray("users").optJSONObject(0).getString("_id");
-                return new SuccessAndResult(id);
+                JSONObject result = resObj.getJSONArray("users").optJSONObject(0);
+                return new SuccessAndResult(result);
             } else return new SuccessAndResult();
         } catch (Exception e) {
-            System.err.println(e);
+            return new SuccessAndResult();
+        }
+    }
+
+    private SuccessAndResult getInfoById (String type, String id) {
+        HttpGet username2idRequest = new HttpGet("https://api.twitch.tv/kraken/" + type + "/" + id);
+        username2idRequest.addHeader("Accept", "application/vnd.twitchtv.v5+json");
+        username2idRequest.addHeader("Client-ID", System.getenv("twitchClient")); // TODO [1]
+
+        try (CloseableHttpResponse res = httpClient.execute(username2idRequest)) {
+            JSONObject result = new JSONObject(EntityUtils.toString(res.getEntity()));
+            return new SuccessAndResult(result);
+        } catch (Exception e) {
             return new SuccessAndResult();
         }
     }
